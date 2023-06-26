994769b2c6a5146d0f344e7f62faa94e6916bd11
Youngwook-Jeon <ela87@naver.com>
2023-06-26 17:20:53 +0900

    Add an order-service project

---
+++ order-service/src/main/java/com/project/young/orderservice/order/domain/OrderRepository.java
@@ -0,0 +1,6 @@
+package com.project.young.orderservice.order.domain;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
+}
---
+++ order-service/src/main/java/com/project/young/orderservice/order/domain/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.project.young.orderservice.order.domain;
+
+public enum OrderStatus {
+    ACCEPTED,
+    REJECTED,
+    DISPATCHED
+}
